f9e61b306cdc716d67f75f4d2e713c86c5bd8b89
Jason Park <jasondevelop@outlook.com>
2016-08-01 22:13:09 +0900

    Add text input/output, variables code.

---
+++ TextInputAndOutput.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+print("Hello\nThis is a message")
+
+x = 2
+y = 3
+print(x, '', y)
+
+name = input("What is your name?\n")
+
+x = int(input("What is x?\n"))
+
+x = float(input("Write a number\n"))
---
+++ Variables.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+x = 2
+price = 2.5
+word = 'Hello'
+word = "Hello"
+word = '''Hello'''
+
+x = 5
+print(x)
+
+y = 3 * x
+print(y)
+
+print("x = " + str(x))
+print("y = " + str(y))
